283eabb457d0011d53113c86f92bb86d968879f6
Giorgio Tognoli <giorgio@f2.net>
2021-12-29 18:11:24 +0100

    new headers links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,25 +1,28 @@
-import React from 'react'
 import { Link } from 'gatsby'
+import React from 'react'
 
 const Header: React.FC = () => (
-  <header className="bg-white sm:h-20 py-5 border-b sm:sticky top-0">
+  <header className="bg-white sm:h-20 py-5 border-b sm:sticky top-0 z-10">
     <div className="max-w-5xl mx-auto px-6">
       <div className="w-full flex flex-col sm:flex-row justify-center sm:justify-between items-center">
         <div className="flex flex-col sm:flex-row items-center mb-4 sm:mb-0">
-          <img
-            src="/react-bricks-logo.svg"
-            className="w-48"
-            alt="React Bricks"
-          />
+          <a href="/">
+            <img
+              src="/react-bricks-logo.svg"
+              className="w-48"
+              alt="React Bricks"
+            />
+          </a>
           <div className="sm:ml-8 flex space-x-5 text-center">
-            <Link to="/" className="text-gray-500 hover:text-pink-700">
-              Home
+            <Link to="/about-us">
+              <a className="text-gray-500 hover:text-pink-700">About us</a>
             </Link>
-            <Link to="/about-us" className="text-gray-500 hover:text-pink-700">
-              About us
+            <Link to="/blog-list-thumbnails">
+              <a className="text-gray-500 hover:text-pink-700">Thumbnails</a>
             </Link>
           </div>
         </div>
+
         <a
           href="/admin"
           className="py-2 px-5 rounded text-white font-medium bg-cyan-500 hover:bg-cyan-600 hover:shadow-lg transition duration-200"
