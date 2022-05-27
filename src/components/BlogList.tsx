e0a589b4cd76ec6fd769948a312f3851374bb3a7
Giorgio Tognoli <giorgio@f2.net>
2022-05-27 17:40:25 +0200

    add interface to layout and bloglist, updated react-bricks-ui

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,4 +1,10 @@
-const BlogList: React.FC = ({ children }) => {
+import { ReactNode } from 'react'
+
+interface BlogListProps {
+  children?: ReactNode
+}
+
+const BlogList: React.FC<BlogListProps> = ({ children }) => {
   return <div className="max-w-6xl mx-auto px-8 pt-16 flex">{children}</div>
 }
 
